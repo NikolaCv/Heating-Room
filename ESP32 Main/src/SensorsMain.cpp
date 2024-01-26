2e61144ae859d8d0a7fe0538973a4eb654d42195
Nikola Cvetanovic <nikola.cvetanovic7@gmail.com>
2024-01-26 21:11:58 +0100

    Complete overhaul

--- ESP32 Main/src/SensorsMain.cpp
+++ ESP32 Main/src/SensorsMain.cpp
@@ -1,10 +1,23 @@
 #include "SensorsMain.h"
 
-void SensorsMain::SetupCurrentSensor(int sensorNumber, ACS712_type sensorType, int sensorPin)
+SensorsMain::SensorsMain(float loadCellCalibrationFactor, float loadCellOffset, float emptyTankWeight, float furnaceOnThreshold, float furnaceOffThreshold)
+: loadCellCalibrationFactor(loadCellCalibrationFactor), loadCellOffset(loadCellOffset), emptyTankWeight(emptyTankWeight),
+  furnaceOnThreshold(furnaceOnThreshold), furnaceOffThreshold(furnaceOffThreshold), currentValues(2, std::vector<float>(windowSize, 0.0f))
+{
+	sht3x = Adafruit_SHT31();
+	furnaceState = false;
+	relayState = false;
+	furnaceStateHasChanged = true;
+	weightAfterFurnaceProcess = weightBeforeFurnaceProcess = 0;
+}
+
+
+void SensorsMain::SetupCurrentSensor(int sensorNumber, ACS712_type sensorType, int sensorPin, float currentOffset)
 {
 	currentSensor[sensorNumber] = ACS712(sensorType, sensorPin);
-	currentSensor[sensorNumber].calibrateAC();
+	currentSensor[sensorNumber].SetCurrentOffset(currentOffset);
 }
+
 
 void SensorsMain::SetupDallasTempSensor(const int sensorPin)
 {
@@ -13,16 +26,43 @@ void SensorsMain::SetupDallasTempSensor(const int sensorPin)
 	dallasSensor.begin();
 }
 
-void SensorsMain::SetupSHT3xTempSensor()
+
+void SensorsMain::SetupSHT3xTempSensor(const int sensorAddress)
 {
-	// TODO
+	sht3xAddress = sensorAddress;
+	sht3x.begin(sht3xAddress);
 }
 
-void SensorsMain::SetupHX711WeightSensor()
+
+void SensorsMain::SetupHX711WeightSensor(const int doutPin, const int sckPin)
 {
-	// TODO
+	loadCell.begin(doutPin, sckPin);
+	loadCell.set_scale(loadCellCalibrationFactor);
+	loadCell.set_offset(loadCellOffset);
 }
-void SensorsMain::SetupRelayPin(const int relayPin)
+
+float SensorsMain::CalculateConsumption()
+{
+	if (weightBeforeFurnaceProcess == 0)
+	{
+		if (furnaceState)
+			weightBeforeFurnaceProcess = GetWeightGrams(10);
+	}
+	else
+	// furnace is working, get measurement when it has finished
+	if (!furnaceState)
+	{
+		weightAfterFurnaceProcess = GetWeightGrams(10);
+		float consumption = weightBeforeFurnaceProcess - weightAfterFurnaceProcess;
+		weightAfterFurnaceProcess = weightBeforeFurnaceProcess = 0;
+		return consumption;
+	}
+
+	return 0;
+}
+
+
+void SensorsMain::SetupRelay(const int relayPin)
 {
 	this->relayPin = relayPin;
 	pinMode(relayPin, OUTPUT);
@@ -30,12 +70,14 @@ void SensorsMain::SetupRelayPin(const int relayPin)
 	UpdateRelayState();
 }
 
-void SensorsMain::ToggleRelay()
+
+void SensorsMain::ToggleRelayState()
 {
 	relayState = !relayState;
 	SetRelayOutput();
 	UpdateRelayState(); // Maybe the change didn't go through, check it
 }
+
 
 void SensorsMain::UpdateRelayState()
 {
@@ -45,6 +87,7 @@ void SensorsMain::UpdateRelayState()
 		relayState = false;
 }
 
+
 void SensorsMain::SetRelayOutput()
 {
 	if(relayState)
@@ -53,38 +96,173 @@ void SensorsMain::SetRelayOutput()
 		digitalWrite(relayPin, LOW);
 }
 
-float SensorsMain::GetCurrent(const int sensorNumber, const int numMeasurements) const
+void SensorsMain::SetFurnaceState()
+{
+	filteredValuesDelta = filteredValues[0] - filteredValues[1];
+	if (filteredValuesDelta > furnaceOnThreshold)
+		{
+			if (!furnaceState) furnaceStateHasChanged = true;
+			furnaceState = true;
+		}
+	else
+		if (filteredValuesDelta < furnaceOffThreshold)
+		{
+			if (furnaceState) furnaceStateHasChanged = true;
+			furnaceState = false;
+		}
+}
+
+bool SensorsMain::FurnaceStateHasChanged()
+{
+	if (furnaceStateHasChanged)
+	{
+		furnaceStateHasChanged = false;
+		return true;
+	}
+	return false;
+}
+
+std::pair<float, float> SensorsMain::GetCurrent(const int sensorNumber, const int numMeasurements)
 {
 	float I = 0;
 
+	// average over numMeasurements, remove 2 potential outliers
+	std::vector<float> measurements;
 	for (int i = 0; i < numMeasurements; ++i)
-		I += currentSensor[sensorNumber].getCurrentAC();
+		measurements.push_back(currentSensor[sensorNumber].getCurrentAC());
+	std::sort(measurements.begin(), measurements.end());
 
-	return I / numMeasurements;
+	if (measurements.size() >= 3)
+		I = std::accumulate(measurements.begin() + 1, measurements.end() - 1, 0.0f) / (numMeasurements - 2);
+	else
+		I = measurements[0];
+
+	currentValues[sensorNumber].push_back(I);
+
+    if (currentValues[sensorNumber].size() > windowSize)
+        currentValues[sensorNumber].erase(currentValues[sensorNumber].begin());
+
+	std::vector<float> temp = currentValues[sensorNumber];
+	
+	std::sort(temp.begin(), temp.end());
+
+	if (temp.size() >= 3)
+		filteredValues[sensorNumber] = std::accumulate(temp.begin() + 1, temp.end() - 1, 0.0f) / (windowSize - 2);
+	
+	SetFurnaceState();
+
+	return std::make_pair(filteredValues[sensorNumber], I);
+}
+
+float SensorsMain::GetFilteredValuesDelta()
+{
+	return filteredValuesDelta;
 }
 
 float SensorsMain::GetDallasTemp(const int numMeasurements)
 {
 	dallasSensor.requestTemperatures();
-	float temp = 0;
+	std::vector<float> measurements(numMeasurements);
 
 	for (int i = 0; i < numMeasurements; ++i)
-		temp += dallasSensor.getTempCByIndex(0);
+		measurements[i] = dallasSensor.getTempCByIndex(0);
 
-	return temp / numMeasurements;
+	std::sort(measurements.begin(), measurements.end());
+
+	return measurements[floor(measurements.size() / 2)];
 }
 
-float* SensorsMain::GetSHT3xTempHumidity(const int numMeasurements) const
+
+std::pair<float, float> SensorsMain::GetSHT3xTempHumidity(const int numMeasurements)
 {
-	return nullptr;
+	std::pair<float, float> result = {-77, -77};
+
+	if (!sht3x.begin())
+	{
+		sht3x.begin(sht3xAddress);
+		return result;
+	}
+
+	float temp = 0;
+	for (int i = 0; i < numMeasurements; ++i)
+		temp += sht3x.readTemperature();
+	result.first = temp / numMeasurements;
+
+	float humidity = 0;
+	for (int i = 0; i < numMeasurements; ++i)
+		humidity += sht3x.readHumidity();
+	result.second = humidity / numMeasurements;
+
+	return result;
 }
 
-float SensorsMain::GetWeight(const int numMeasurements) const
+
+// Returns median of numMeasurements of 3 measurements
+float SensorsMain::GetWeightGrams(const int numMeasurements)
 {
-	return 0;
+	std::vector<float> measurements(numMeasurements);
+
+	for (int i = 0; i < numMeasurements; ++i)
+		measurements[i] = loadCell.get_units(3);
+
+	std::sort(measurements.begin(), measurements.end());
+
+	return measurements[floor(measurements.size() / 2)];
+}
+
+
+float SensorsMain::GetPelletWeightGrams(const int numMeasurements)
+{
+	return GetWeightGrams(numMeasurements) - emptyTankWeight;
+}
+
+
+float SensorsMain::GetLoadCellCalibrationFactor() const
+{
+	return loadCellCalibrationFactor;
+}
+
+
+void SensorsMain::SetConfig(float newLoadCellCalibrationFactor, float newLoadCellOffset, float newEmptyTankWeight, float newFurnaceOnThreshold, float newFurnaceOffThreshold)
+{	
+	loadCellCalibrationFactor = newLoadCellCalibrationFactor;
+	loadCellOffset = newLoadCellOffset;
+	emptyTankWeight = newEmptyTankWeight;
+	furnaceOnThreshold = newFurnaceOnThreshold;
+	furnaceOffThreshold = newFurnaceOffThreshold;
+}
+
+
+void SensorsMain::ResetConfig()
+{
+	loadCellCalibrationFactor = DefaultConfig::loadCellCalibrationFactor;
+	loadCellOffset = DefaultConfig::loadCellOffset;
+	emptyTankWeight = DefaultConfig::emptyTankWeight;
+	furnaceOnThreshold = DefaultConfig::furnaceOnThreshold;
+	furnaceOffThreshold = DefaultConfig::furnaceOffThreshold;
+}
+
+StaticJsonDocument<200> SensorsMain::GetConfig() const
+{
+	StaticJsonDocument<200> sensorConfig;
+	
+    sensorConfig["loadCellCalibrationFactor"] = loadCellCalibrationFactor;
+    sensorConfig["loadCellOffset"] = loadCellOffset;
+    sensorConfig["emptyTankWeight"] = emptyTankWeight;
+
+    sensorConfig["furnaceOnThreshold"] = furnaceOnThreshold;
+    sensorConfig["furnaceOffThreshold"] = furnaceOffThreshold;
+
+    return sensorConfig;
 }
 
 bool SensorsMain::GetRelayState() const
 {
-	return relayState;
+	return int(relayState);
+}
+
+
+int SensorsMain::GetFurnaceState() const
+{
+	return int(furnaceState);
 }
--- ESP32 Main/src/SensorsMain.h
+++ ESP32 Main/src/SensorsMain.h
@@ -4,43 +4,75 @@
 #include <Adafruit_SHT31.h>
 #include <HX711.h>
 #include <OneWire.h>
+#include <Wire.h>
 #include <DallasTemperature.h>
 #include <SPI.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
+#include <vector>
+#include <numeric>
+#include <algorithm>
 
 #include "ACS712.h"
+#include "Configuration/DefaultConfig.h"
 
 class SensorsMain 
 {
 	public:
-		void SetupRelayPin(int relayPin);
-		void SetupCurrentSensor(int sensorNumber, ACS712_type sensorType, int sensorPin);
+		SensorsMain(float loadCellCalibrationFactor, float loadCellOffset, float emptyTankWeight, float furnaceOnThreshold, float furnaceOffThreshold);
+		void SetupRelay(int relayPin);
+		void SetupCurrentSensor(int sensorNumber, ACS712_type sensorType, int sensorPin, float currentOffset);
 		void SetupDallasTempSensor(const int sensorPin);
-		void SetupSHT3xTempSensor();
-		void SetupHX711WeightSensor();
+		void SetupSHT3xTempSensor(const int sensorAddress);
+		void SetupHX711WeightSensor(const int doutPin, const int sckPin);
+
+		float CalculateConsumption();
 		
-		void ToggleRelay();
+		void ToggleRelayState();
 		void UpdateRelayState();
+
 		void SetRelayOutput();
+		void SetFurnaceState();
+		bool FurnaceStateHasChanged();
 
 		bool GetRelayState() const;
-		float GetCurrent(const int sensorNumber, const int numMeasurements) const;
+		int GetFurnaceState() const;
+		std::pair<float, float> GetCurrent(const int sensorNumber, const int numMeasurements);
+		float GetFilteredValuesDelta();
 		float GetDallasTemp(const int numMeasurements);
-		float* GetSHT3xTempHumidity(const int numMeasurements) const;
-		float GetWeight(const int numMeasurements) const;
+		std::pair<float, float> GetSHT3xTempHumidity(const int numMeasurements);
+		float GetWeightGrams(const int numMeasurements);
+		float GetPelletWeightGrams(const int numMeasurements);
+		float GetLoadCellCalibrationFactor() const;
+
+		void SetConfig(float newLoadCellCalibrationFactor, float newLoadCellOffset, float newEmptyTankWeight, float newFurnaceOnThreshold, float newFurnaceOffThreshold);
+		void ResetConfig();
+		StaticJsonDocument<200> GetConfig() const;
+
 	private:
-		//Adafruit_SHT31 sht31 = Adafruit_SHT31();
-		//HX711 scale;
+		Adafruit_SHT31 sht3x;
+		int sht3xAddress;
+		HX711 loadCell;
+  		float loadCellCalibrationFactor, loadCellOffset;
+		float emptyTankWeight;
+		float weightBeforeFurnaceProcess, weightAfterFurnaceProcess;
 
 		ACS712 currentSensor[2];
+		std::vector<std::vector<float>> currentValues;
+		float filteredValues[2];
+		float filteredValuesDelta;
+		int windowSize = 10;
+
+		bool furnaceState;
+		bool furnaceStateHasChanged;
+		float furnaceOnThreshold, furnaceOffThreshold;
 
 		OneWire oneWire;
 		DallasTemperature dallasSensor;
 
 		int relayPin;
-		bool relayState = false; // false = LOW, true = HIGH
+		bool relayState; // false = LOW, true = HIGH
 };
 
 #endif

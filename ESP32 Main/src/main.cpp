aeeed36962d31367e1a4853260cbef010493b296
Nikola Cvetanovic <nikola.cvetanovic7@gmail.com>
2024-01-26 21:14:13 +0100

    Add remaining sensor data acquisition and publishing

--- ESP32 Main/src/main.cpp
+++ ESP32 Main/src/main.cpp
@@ -17,11 +17,15 @@
 
 #define SHT3X_ADDRESS 0x44
 #define LOAD_CELL_DOUT 35
-#define LOAD_CELL_SCK 34
+#define LOAD_CELL_SCK 32
 #define RELAY_PIN 17
 #define DS18B20_PIN 33
-#define ACS712_1_PIN 32
+#define ACS712_1_PIN 34
 #define ACS712_2_PIN 39
+
+float currentOffset1 = 1.0035;
+float currentOffset2 = 0.9855;
+
 /*
 // mSD card reader pins
 #define SD_CS_PIN 5
@@ -30,36 +34,14 @@
 #define SD_SCK_PIN 18
 */
 
-
-/* TODO
-
-topics:
-Prilikom reseta/update treba da se prikazu vrednosti u HA (3 prikaza mozda, default, current, set new)
-request => main update, main prosledi klapni => klapna update i vrati => main publish na heating_room/config
-
-TODO
-u svakom get sensor, i u svakom setup sensor treba proveriti da li je senzor prikljucen
-(osim za ACS712, i mozda za Dallas)
-
-*/
-
 void ReconnectWiFi();
 void SetupSensors(SensorsMain& sensors);
 void PublishSensorData(MqttCommunication& mqtt, SensorsMain& sensors);
 
-//Adafruit_SHT31 sht31 = Adafruit_SHT31();
-//HX711 scale;
-
-SensorsMain sensors;
+SensorsMain sensors(DefaultConfig::loadCellCalibrationFactor, DefaultConfig::loadCellOffset, DefaultConfig::emptyTankWeight,
+					DefaultConfig::furnaceOnThreshold, DefaultConfig::furnaceOffThreshold);
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
-
-MqttCommunication mqtt(mqttClient, Secrets::mqttServerIP, Secrets::mqttPort,
-					   "ESP32 Heating Room", Secrets::mqttUserName, Secrets::mqttUserPassword,
-					   DefaultConfig::samplingRateSeconds, sensors, Serial,
-					   "heating_room/relay/control", // sub
-					   "heating_room/config/reset", // sub
-					   "heating_room/config/update"); // sub
 
 EspMainSerialCommunication serialComm(DefaultConfig::vibrationResetMqttSeconds, millis(),
 									  "heating_room/flap/vibration", // pub
@@ -67,10 +49,22 @@ EspMainSerialCommunication serialComm(DefaultConfig::vibrationResetMqttSeconds, 
 									  "heating_room/flap/position", // pub
 									  "heating_room/config"); // pub
 
+MqttCommunication mqtt(mqttClient, Secrets::mqttServerIP, Secrets::mqttPort,
+					   "ESP32 Heating Room", Secrets::mqttUserName, Secrets::mqttUserPassword,
+					   DefaultConfig::samplingRateSeconds, sensors, Serial,
+					   serialComm,
+					   "heating_room/relay/state", // pub
+					   "heating_room/relay/control", // sub
+					   "heating_room/config/reset", // sub
+					   "heating_room/config/update"); // sub
+
+
+
 unsigned long currTime = millis();
 
 void setup()
 {	
+	delay(3000);
 	Serial.begin(9600);
 	ReconnectWiFi();
 
@@ -80,38 +74,74 @@ void setup()
 	Serial.print('G'); // Get values from ESP Flap Observer, which triggers config values to be published
 }
 
+
 void loop()
 {
 	currTime = millis();
-	Serial.println(currTime);
+	
 	ReconnectWiFi();
 
 	mqtt.Reconnect();
 	mqtt.Loop();
 	mqtt.PublishDataPeriodically(PublishSensorData, sensors);
 
-	serialComm.ReadFromSerial(Serial, mqtt, currTime);
+	serialComm.ReadFromSerial(Serial, mqtt, sensors, currTime);
+
+	//Serial.println(sensors.GetWeightGrams(1)); // For Calibration
+
+	delay(25);
 }
+
 
 void SetupSensors(SensorsMain& sensors)
 {
-	sensors.SetupCurrentSensor(0, ACS712_30A, ACS712_1_PIN);
-	sensors.SetupCurrentSensor(1, ACS712_30A, ACS712_2_PIN);
+	sensors.SetupCurrentSensor(0, ACS712_30A, ACS712_1_PIN, currentOffset1);
+	sensors.SetupCurrentSensor(1, ACS712_30A, ACS712_2_PIN, currentOffset2);
 	sensors.SetupDallasTempSensor(DS18B20_PIN);
-	sensors.SetupRelayPin(RELAY_PIN);
+	sensors.SetupRelay(RELAY_PIN);
+	sensors.SetupSHT3xTempSensor(SHT3X_ADDRESS);
+	sensors.SetupHX711WeightSensor(LOAD_CELL_DOUT, LOAD_CELL_SCK);
 }
+
 
 void PublishSensorData(MqttCommunication& mqtt, SensorsMain& sensors)
 {
-	mqtt.PublishMessage("heating_room/burner", String(sensors.GetCurrent(0, 10), 2));
-	mqtt.PublishMessage("heating_room/pump", String(sensors.GetCurrent(1, 10), 2));
-	
-	// blocking function, potentially split into 2 functions (requestTemperatures(), getTempCByIndex())
-	// first requestTemp, then after 1 second getTemp (then delay every other sensor, so all publish at the same time?)
-	mqtt.PublishMessage("heating_room/furnace/temp", String(sensors.GetDallasTemp(1), 2));
+	std::pair<float, float> result = sensors.GetCurrent(0, 12);
+	mqtt.PublishMessage("heating_room/burner/filtered", String(result.first, 3));
+	mqtt.PublishMessage("heating_room/burner/raw", String(result.second, 3));
+
+	result = sensors.GetCurrent(1, 12);
+	mqtt.PublishMessage("heating_room/pump/filtered", String(result.first, 3));
+	mqtt.PublishMessage("heating_room/pump/raw", String(result.second, 3));
+
+	mqtt.PublishMessage("heating_room/burner_minus_pump/filtered", String(sensors.GetFilteredValuesDelta(), 3));
+
+	if (sensors.FurnaceStateHasChanged())
+	{
+		mqtt.PublishMessage("heating_room/furnace/state", String(1 - sensors.GetFurnaceState()));
+		mqtt.PublishMessage("heating_room/furnace/state", String(sensors.GetFurnaceState()));
+	}
+	// Dallas blocking function, potentially split into 2 functions (requestTemperatures(), getTempCByIndex())
+	float temp = sensors.GetDallasTemp(5);
+	if (temp > 0) mqtt.PublishMessage("heating_room/furnace/temp", String(temp, 2));
+
+
+	result = sensors.GetSHT3xTempHumidity(1);
+	if(result.first != -77)
+	{
+		mqtt.PublishMessage("heating_room/outside/temp", String(result.first, 2));
+		mqtt.PublishMessage("heating_room/outside/humidity", String(result.second, 2));
+	}
+
+	float weight = sensors.GetPelletWeightGrams(8)/1000.0;
+	if (weight > -5) mqtt.PublishMessage("heating_room/tank", String(weight, 3));
+
+	float consumption = sensors.CalculateConsumption()/1000.0;
+	if (consumption) mqtt.PublishMessage("heating_room/consumption", String(consumption, 3));
 }
 
-// Rewrite for offline data collecting
+
+// Rewrite for offline data collecting if needed
 void ReconnectWiFi() 
 {
 	if(WiFi.status() != WL_CONNECTED)
